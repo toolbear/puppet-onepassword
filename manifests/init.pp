43dc9d8ad0c306b74b673a3d6541c4f7ec6aad4b
Tim Taylor <tim@tool-man.org>
2014-02-24 18:01:27 -0800

    Update 1Password to v4.2.1

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,12 @@
 #
 # Usage:
 #
-#     include 1password
+#     include onepassword
 class onepassword {
   $host = 'd13itkw33a7sus.cloudfront.net'
-  $path = 'dist/1P/mac4/1Password-4.0.9.zip'
+  $path = 'dist/1P/mac4/1Password-4.2.1.zip'
 
-  package { '1Password':
+  package { '1Password 4':
     source   => "https://${host}/${path}",
     provider => 'compressed_app'
   }
