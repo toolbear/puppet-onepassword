f44fc27c9216611c1df685163059dd8c0f2c1598
Tim Taylor <tim@tool-man.org>
2014-02-24 18:01:27 -0800

    Update 1Password version
    Latest stable version is 4.1.3

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,10 @@
 #
 # Usage:
 #
-#     include 1password
+#     include onepassword
 class onepassword {
   $host = 'd13itkw33a7sus.cloudfront.net'
-  $path = 'dist/1P/mac4/1Password-4.0.9.zip'
+  $path = 'dist/1P/mac4/1Password-4.1.3.zip'
 
   package { '1Password':
     source   => "https://${host}/${path}",
